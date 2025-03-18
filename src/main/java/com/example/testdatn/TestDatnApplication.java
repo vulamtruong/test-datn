bd4d66a29c33c04fcdd7f90a82a20fb3c95773f9
VuLamTruong <vulamtruong666@gmail.com>
2025-03-18 23:05:03 +0700

    ocee

---
+++ src/main/java/com/example/testdatn/TestDatnApplication.java
@@ -0,0 +1,13 @@
+package com.example.testdatn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestDatnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestDatnApplication.class, args);
+    }
+
+}
